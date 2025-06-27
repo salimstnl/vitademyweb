65c97644aecdb7d6d6c26c7b2bab2c03bb9b31fc
Taikez <juliannardita@gmail.com>
2025-06-27 16:52:06 +0700

    fix slug module

--- src/app/(user)/learn/modules/[slug]/page.tsx
+++ src/app/(user)/learn/modules/[slug]/page.tsx
@@ -1,15 +1,21 @@
-"use client";
-import { useSelectedLayoutSegment } from "next/navigation";
+// "use client";
+// import { useSelectedLayoutSegment } from "next/navigation";
 
-// async function getModule(slug) {
-//   const res =
+// // async function getModule(slug) {
+// //   const res =
+// // }
+
+// export default function ModulesDetail({ slug }: { slug: string }) {
+//   const segment = useSelectedLayoutSegment();
+//   return (
+//     <div>
+//       <h1>{slug}</h1>
+//     </div>
+//   );
 // }
 
-export default function ModulesDetail({ slug }: { slug: string }) {
-  const segment = useSelectedLayoutSegment();
-  return (
-    <div>
-      <h1>{slug}</h1>
-    </div>
-  );
+import React from "react";
+
+export default function page() {
+  return <div>page</div>;
 }
